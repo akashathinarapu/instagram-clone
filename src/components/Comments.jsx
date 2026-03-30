fd596b0684331d23177c939e236cc3fcb7211c99
akashathinarapu <akashathinarapu@gmail.com>
2026-03-30 14:40:14 +0530

    Add Comments component for post comments section

---
+++ src/components/Comments.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Comments = () => {
+    return (
+        <div>
+            <h2>Comments</h2>
+            {/* Comment section UI */}
+        </div>
+    );
+};
+
+export default Comments;
